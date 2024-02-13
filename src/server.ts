3a4a0d20237a021725aeb1731ebbdd7bd00af6e6
Evan Hicks <hicks2evan@gmail.com>
2024-02-12 19:29:39 -0700

    Clean up main GET response

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express, {Express, Request, Response} from 'express';
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response)=>{
-    res.send('Hello, this is color separator, not implemented');
+    res.send('Welcome to Color Separator API, you may want to look back at https://github.com/hicks2evan/color-separator to download a postman collection.');
 });
 
 app.listen(process.env.PORT || 3001, ()=> {
