c0b7bc61aa27bd9bb9fceca1eca8c7e503c7535c
Evan Hicks <hicks2evan@gmail.com>
2024-02-12 19:13:34 -0700

    Add heroku procfile

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import express, {Express, Request, Response} from 'express';
 
 const app: Express = express();
-const port = 3001;
 
 app.get('/', (req: Request, res: Response)=>{
     res.send('Hello, this is color separator, not implemented');
 });
 
-app.listen(port, ()=> {
-console.log(`[Server]: I am running at https://localhost:${port}`);
+app.listen(process.env.PORT || 3001, ()=> {
 });
